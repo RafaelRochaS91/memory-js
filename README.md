d4e745cfd6d84535fa21b25ab85c16d46d6a1d10
Rafael Rocha <rafael.rocha@mindera.com>
2018-10-24 18:10:39 +0100

    Added timer logic and updated Readme.MD

---
+++ README.md
@@ -0,0 +1,19 @@
+# Instructions
+
+## Setup
+* Unzip the folder then
+
+      $ npm install
+
+## Starting
+Start image-server:
+
+      $ npm run image-server
+
+Start parcel-bundler:
+
+      $ npm start
+
+Then visit [localhost](http://localhost:1234)
+
+## Notes
--- app/src/models/Player.js
+++ app/src/models/Player.js
@@ -1,6 +1,10 @@
 const initialPlayerStatus = {
 	numberOfPlays: 0,
-	timeElapsed: 0,
+	timer: {
+		seconds: 0,
+		minutes: 0,
+		interval: () => null
+	},
 	finalScore: 0,
 };
 
@@ -10,15 +14,27 @@ export class Player {
 	 * @param {number} difficulty
 	 */
 	constructor(difficulty) {
-		this.status = initialPlayerStatus;
+		this.status = { ...initialPlayerStatus, difficulty };
 	}
 
 	startTimer() {
-		const { timeElapsed } = this.status;
+		const { minutes, seconds } = this.status.timer;
+		let timerContainer = document.getElementById('#timer');
+
+		setInterval(() => {
+			timerContainer.innerHTML = minutes + 'mins ' + seconds + 'secs';
+			seconds++;
+
+			if (seconds === 60) {
+				minutes++;
+				seconds = 0;
+			}
+		}, 1000);
 	}
 
 	stopTimer() {
 		const { timeElapsed } = this.status;
+
 	}
 
 	increaseNumberOfPlays() {
